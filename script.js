486f2e667433f0205a58395ebeb70068fea74c24
Christopher Chrun <chrischrun@gmail.com>
2023-12-23 21:15:00 -0800

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const hangmanImage = document.querySelector(".hangman-box img");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = gameModal.querySelector("button");
+
---
+++ word-list.js
@@ -0,0 +1,10 @@
+const wordList = [
+    {
+        word: "mouth",
+        hint: "Part of the body you want your girlfriend to close most of the time."
+    },
+    {
+        word: "oxygen",
+        hint: "Something you go out and get after being yelled at by your girlfriend."
+    },
+];
