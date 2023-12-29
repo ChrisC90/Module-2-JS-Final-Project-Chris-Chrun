7a98c7a4e8b3c0efe3a2732a9739b0072e428a32
Christopher Chrun <chrischrun@gmail.com>
2023-12-28 19:32:58 -0800

    commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+// update the hangman image when guessed wrong //
+function updateHangmanImage() {
+    const hangmanImg = document.getElementById('hangman-img');
+    hangmanImg.src = hangmanImages[wrongGuesses];
+}
+
+// event listener for when the DOM content is fully loaded //
 document.addEventListener('DOMContentLoaded', function () {
     const words = [
         { word: 'mouth', hint: 'Something you want your GIRLFRIEND to close most of the time!' },
         { word: 'oxygen', hint: 'Something you go out and get when getting yelled at by GIRLFRIEND.' },
     ];
 
+    // store the selected word, guessed word, and wrong guesses //
     let selectedWord, guessedWord, wrongGuesses;
 
     const hangmanImages = [
@@ -22,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
         'zxcvbnm',
     ];
 
+    // DOM elements //
     const hangmanContainer = document.getElementById('hangman-container');
     const hangmanImage = document.getElementById('hangman-image');
     const wordDisplay = document.getElementById('word-display');
@@ -32,32 +41,40 @@ document.addEventListener('DOMContentLoaded', function () {
     const guessesText = document.getElementById('guesses-text');
     const maxGuesses = 6;
 
+    // to start the game //
     function startGame() {
+        // selecting random word //
         selectedWord = words[Math.floor(Math.random() * words.length)];
         guessedWord = Array(selectedWord.word.length).fill('_');
         wrongGuesses = 0;
 
+        // updating display //
         updateHangmanImage();
         updateWordDisplay();
         createKeyboard();
         displayHint();
 
+        // reset messages and hide the reset button //
         message.textContent = '';
         resetButton.style.display = 'none';
+
+        // guesses remaining display //
         updateGuessesRemaining();
     }
 
+    // update the hangman image when making wrong guesses //
     function updateHangmanImage() {
         hangmanImage.style.backgroundImage = `url('${hangmanImages[wrongGuesses]}')`;
     }
 
+    // update the word display guessed letters //
     function updateWordDisplay() {
         wordDisplay.textContent = guessedWord.join(' ');
     }
 
+    // function to create the keyboard buttons //
     function createKeyboard() {
         keyboard.innerHTML = '';
-        // Create buttons for QWERTY layout
         qwertyLayout.forEach(row => {
             const rowDiv = document.createElement('div');
             rowDiv.classList.add('keyboard-row');
@@ -71,60 +88,74 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    // handle a guessed letter //
     function handleGuess(event) {
         const guessedLetter = event.target.textContent.toLowerCase();
         if (selectedWord.word.includes(guessedLetter)) {
+            // update guessedWord with the correctly guessed letter //
             for (let i = 0; i < selectedWord.word.length; i++) {
                 if (selectedWord.word[i] === guessedLetter) {
                     guessedWord[i] = guessedLetter;
                 }
             }
+            // check if the word is fully guessed //
             if (!guessedWord.includes('_')) {
                 gameWon();
             }
         } else {
+            // incrementing wrongGuesses and updating hangman image //
             wrongGuesses++;
             updateHangmanImage();
+            // check if the maximum number of wrong guesses is reached //
             if (wrongGuesses === maxGuesses) {
                 gameOver();
             }
         }
 
+        // disable the clicked button and updating word display and guesses remaining //
         event.target.disabled = true;
         updateWordDisplay();
         updateGuessesRemaining();
     }
 
+    // handle the game won scenario //
     function gameWon() {
         message.textContent = 'Congratulations! You guessed the word!';
         endGame();
     }
 
+    // handle the game over scenario //
     function gameOver() {
         message.textContent = `Sorry! The correct word was "${selectedWord.word}".`;
         endGame();
     }
 
+    // handle the end of the game //
     function endGame() {
         disableKeyboard();
         resetButton.style.display = 'block';
     }
 
+    // disabling all keyboard buttons //
     function disableKeyboard() {
         keyboard.querySelectorAll('button').forEach(button => {
             button.disabled = true;
         });
     }
 
+    // displaying the hint //
     function displayHint() {
         hintText.textContent = `Hint: ${selectedWord.hint}`;
     }
 
+    // updating the display of remaining guesses //
     function updateGuessesRemaining() {
         guessesText.textContent = `Guesses remaining: ${maxGuesses - wrongGuesses}`;
     }
 
+    // event listener for the reset button //
     resetButton.addEventListener('click', startGame);
 
+    // start the game when the page loads //
     startGame();
 });
